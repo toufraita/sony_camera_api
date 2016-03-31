82d8637ee176b477fa9e50db1e8489bf34c2e29c
marc <marc@peruzzetto.eu>
2016-03-31 18:42:36 +0200

    Add a new module with the specific functions for the X1000V + test of speed for deleting files (about 10s to delete 100files).

---
+++ src/x1000_commands.py
@@ -0,0 +1,9 @@
+__author__ = 'marc'
+
+"""
+Specific functions to control the X1000 camera to take pictures every 1 second and recover/delete them.
+"""
+
+import pysony
+import time
+import urllib2
